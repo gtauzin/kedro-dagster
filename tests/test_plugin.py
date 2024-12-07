5e5eafdd1d645e5de7a5bdf788bfae7818b0d8d0
Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>
2024-12-07 19:21:22 +0100

    Fix CI
    Signed-off-by: Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -7,5 +7,4 @@ def test_dagster_init(cli_runner, metadata):
     """Check the generation and validity of a simple Airflow DAG."""
     command = "dagster init"
     result = cli_runner.invoke(commands, command, obj=metadata)
-
     assert result.exit_code == 0, (result.exit_code, result.stdout)
