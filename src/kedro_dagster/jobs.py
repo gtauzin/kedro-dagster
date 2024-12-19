daf9d51c0bafc4f97adb43dd8b85aa48da85ab12
Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>
2024-12-19 16:48:59 +0100

    Try GraphDefinition
    Signed-off-by: Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>

--- src/kedro_dagster/jobs.py
+++ src/kedro_dagster/jobs.py
@@ -3,18 +3,15 @@
 from typing import Any
 
 from dagster import (
-    AssetIn,
     AssetKey,
-    AssetOut,
     AssetSpec,
+    DependencyDefinition,
+    ExecutorDefinition,
+    GraphDefinition,
     HookContext,
-    HookDefinition,
     In,
     JobDefinition,
-    Nothing,
     failure_hook,
-    graph,
-    multi_asset,
     op,
 )
 from kedro import __version__ as kedro_version
@@ -27,16 +24,15 @@ from kedro_dagster.utils import _include_mlflow
 
 
 def get_job_from_pipeline(
-    before_pipeline_run_hook: HookDefinition,
     asset_input_dict: dict[str, AssetSpec],
     multi_asset_node_dict: dict[str, Any],
     op_node_dict: dict[str, Any],
     pipeline: Pipeline,
     catalog: DataCatalog,
     job_name: str,
-    job_config: dict,
     hook_manager,
     run_params: dict[str, Any],
+    executor: ExecutorDefinition,
 ) -> JobDefinition:
     """Create a Dagster job from a Kedro ``Pipeline``.
 
@@ -47,13 +43,24 @@ def get_job_from_pipeline(
         catalog: An implemented instance of ``CatalogProtocol``
         from which to fetch data.
         job_name: The name of the job.
-        job_config: The configuration of the job.
         hook_manager: The ``PluginManager`` to activate hooks.
         run_params: The parameters of the run.
+        executor: .
 
     Returns:
         JobDefinition: A Dagster job.
     """
+
+    @op(
+        name=f"{job_name}_before_pipeline_run_hook",
+        description="Hook to be executed before a pipeline run.",
+    )
+    def before_pipeline_run_hook():
+        hook_manager.hook.before_pipeline_run(
+            run_params=run_params,
+            pipeline=pipeline,
+            catalog=catalog,
+        )
 
     @op(
         name=f"{job_name}_after_pipeline_run_hook",
@@ -84,87 +91,53 @@ def get_job_from_pipeline(
             catalog=catalog,
         )
 
-    # TODO: Why does the graph not appear on Dagster UI?
-    ins = {}
-    for asset_name in pipeline.inputs():
-        if not asset_name.startswith("params:"):
-            ins[asset_name] = AssetIn(
-                key=asset_name,
-                input_manager_key=f"{asset_name}_io_manager",
-            )
+    def get_node_def(name):
+        if name in multi_asset_node_dict:
+            return multi_asset_node_dict[name].node_def
 
-    @graph(
-        name=job_name,
-        description=f"Graph derived from pipeline associated to the `{job_name}` job.",
-        # ins=ins,
-        out=None,
-        tags=None,
-        config=None,
-    )
-    def pipeline_graph():
-        materialized_before_pipeline_run_hook_outputs = before_pipeline_run_hook()
+        return op_node_dict[name]
 
-        materialized_assets = {
-            materialized_output.output_name: materialized_output
-            for materialized_output in materialized_before_pipeline_run_hook_outputs
+    pipeline_node_defs = [before_pipeline_run_hook, after_pipeline_run_hook] + [
+        get_node_def(node.name) for node in pipeline.nodes
+    ]
+
+    output_node_map = {
+        asset_name: [node for node in pipeline.nodes if asset_name in node.outputs][0]
+        for asset_name in pipeline.all_outputs()
+    }
+
+    pipeline_dependencies = {
+        f"{job_name}_before_pipeline_run_hook": {},
+        f"{job_name}_after_pipeline_run_hook": {
+            asset_name: DependencyDefinition(output_node_map[asset_name].name, asset_name)
+            for asset_name in pipeline.outputs()
+        },
+    }
+    for node in pipeline.nodes:
+        pipeline_dependencies |= {
+            node.name: {
+                "before_pipeline_run_hook_result": DependencyDefinition(f"{job_name}_before_pipeline_run_hook"),
+            }
+            | {
+                asset_name: DependencyDefinition(output_node_map[asset_name].name, asset_name)
+                for asset_name in node.inputs
+                if asset_name not in pipeline.inputs()
+            }
         }
 
-        for layer in pipeline.grouped_nodes:
-            for node in layer:
-                if node.name in multi_asset_node_dict:
-                    if len(node.outputs):
-                        op = multi_asset_node_dict[node.name]
-                    else:
-                        op = op_node_dict[node.name]
-
-                    node_inputs = node.inputs
-                    node_inputs.append(f"{node.name}_before_pipeline_run_hook")
-
-                    materialized_input_assets = {
-                        input_name: asset_input_dict[input_name]
-                        for input_name in node_inputs
-                        if input_name in asset_input_dict
-                    }
-
-                    materialized_input_assets |= {
-                        input_name: materialized_assets[input_name]
-                        for input_name in node_inputs
-                        if input_name in materialized_assets
-                    }
-
-                    materialized_outputs = op(**materialized_input_assets)
-
-                    if len(node.outputs) == 1:
-                        materialized_output_assets = {materialized_outputs.output_name: materialized_outputs}
-                    elif len(node.outputs) > 1:
-                        materialized_output_assets = {
-                            materialized_output.output_name: materialized_output
-                            for materialized_output in materialized_outputs
-                        }
-
-                    materialized_assets |= materialized_output_assets
-
-        run_results = {asset_name: materialized_assets[asset_name] for asset_name in pipeline.outputs()}
-
-        after_pipeline_run_hook(**run_results)
-
-    job = pipeline_graph.to_job(
+    # TODO: Why does the graph not appear on Dagster UI?
+    pipeline_graph = GraphDefinition(
         name=job_name,
-        description="",
-        resource_defs=None,
-        config=job_config.get("config", None),
-        tags=job_config.get("tags", None),
-        metadata=job_config.get("metadata", None),
-        logger_defs=job_config.get("logger_defs", None),
-        executor_def=job_config.get("executor_def", None),
-        op_retry_policy=job_config.get("op_retry_policy", None),
-        partitions_def=job_config.get("partitions_def", None),
-        asset_layer=job_config.get("asset_layer", None),
-        input_values=job_config.get("input_values", None),
-        run_tags=job_config.get("run_tags", None),
-        hooks={on_pipeline_error_hook},
-        _asset_selection_data=None,
-        op_selection=None,
+        description=f"Graph derived from pipeline associated to the `{job_name}` job.",
+        node_defs=pipeline_node_defs,
+        dependencies=pipeline_dependencies,
+    )
+
+    job = JobDefinition(
+        name=job_name,
+        graph_def=pipeline_graph,
+        executor_def=executor,
+        hook_defs={on_pipeline_error_hook},
     )
 
     return job
@@ -175,12 +148,13 @@ def load_jobs_from_kedro_config(
     asset_input_dict: dict,
     multi_asset_node_dict: dict,
     op_node_dict: dict,
+    executors: dict[str, ExecutorDefinition],
     catalog: DataCatalog,
     hook_manager,
     session_id: str,
     project_path: str,
     env: str,
-) -> tuple[list[JobDefinition], HookDefinition]:
+) -> list[JobDefinition]:
     """Loads job definitions from a Kedro pipeline.
 
     Args:
@@ -188,6 +162,7 @@ def load_jobs_from_kedro_config(
         asset_input_dict: A dictionary of asset inputs.
         multi_asset_node_dict: A dictionary of multi-asset nodes.
         op_node_dict: A dictionary of operation nodes.
+        executors: A dictionary of executors.
         catalog: An implemented instance of ``CatalogProtocol``
         from which to fetch data.
         hook_manager: The ``PluginManager`` to activate hooks.
@@ -197,33 +172,8 @@ def load_jobs_from_kedro_config(
 
     Returns:
         List[JobDefintion]: A list of dagster job definitions.
-        HookDefintion: The Kedro ``before_pipeline_run`` hook translated
-        into a Dagster definition.
 
     """
-
-    @multi_asset(
-        name="before_pipeline_run_hook",
-        group_name="hooks",
-        description="Hook to be executed before a pipeline run.",
-        outs={
-            f"{node.name}_before_pipeline_run_hook": AssetOut(
-                key=f"{node.name}_before_pipeline_run_hook",
-                description=f"Untangible asset corresponding to `{node.name}` for the `before_pipeline_run` hook.",
-                dagster_type=Nothing,
-                is_required=False,
-            )
-            for node in pipelines["__default__"].nodes
-        },
-    )
-    def before_pipeline_run_hook():
-        hook_manager.hook.before_pipeline_run(
-            run_params=run_params,
-            pipeline=pipeline,
-            catalog=catalog,
-        )
-
-        return tuple([None] * len(pipelines["__default__"].nodes))
 
     jobs = []
     for job_name, job_config in dagster_config.jobs.items():
@@ -254,8 +204,14 @@ def load_jobs_from_kedro_config(
 
         pipeline = pipelines.get(pipeline_name).filter(**filter_params)
 
+        executor_config = job_config.executor
+        if isinstance(executor_config, str):
+            if executor_config in executors:
+                executor = executors[executor_config]
+            else:
+                raise ValueError("")
+
         job = get_job_from_pipeline(
-            before_pipeline_run_hook=before_pipeline_run_hook,
             asset_input_dict=asset_input_dict,
             multi_asset_node_dict=multi_asset_node_dict,
             op_node_dict=op_node_dict,
@@ -263,10 +219,10 @@ def load_jobs_from_kedro_config(
             catalog=catalog,
             hook_manager=hook_manager,
             job_name=job_name,
-            job_config=job_config.pipeline.model_dump(),
             run_params=run_params,
+            executor=executor,
         )
 
         jobs.append(job)
 
-    return jobs, before_pipeline_run_hook
+    return jobs
