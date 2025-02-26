322802cd39353a749c7d852a83778a13899da058
Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>
2025-02-26 09:23:59 +0100

    Update template
    Signed-off-by: Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>

--- src/kedro_dagster/templates/definitions.py
+++ src/kedro_dagster/templates/definitions.py
@@ -1,34 +1,37 @@
 """Dagster definitions."""
 
+import os
+
 import dagster as dg
 from dagster_aws.s3 import S3PickleIOManager, S3Resource
-from kedro_dagster import (
-    translate_kedro,
-)
+from kedro_dagster import KedroDagsterTranslator
 
-ENV = "base"
+KEDRO_ENV = os.getenv("KEDRO_ENV", "dev")
 
-assets, resources, jobs, schedules, loggers, executors = translate_kedro(env=ENV)
+translator = KedroDagsterTranslator(env=KEDRO_ENV)
+translator.translate()
 
+resources = translator.named_resources
 # The "io_manager" key handles how Kedro MemoryDatasets are handled by Dagster
-if ENV == "base":
+if KEDRO_ENV == "base":
     resources |= {
         "io_manager": dg.fs_io_manager,
     }
-elif ENV == "dev":
+elif KEDRO_ENV == "dev":
     resources |= {
         "io_manager": S3PickleIOManager(
             s3_resource=S3Resource(),
-            s3_bucket="<MY_BUCKET>",
-            s3_prefix="<MY_PREFIX>",
+            s3_bucket="zaphiro-dagster-demo",
+            s3_prefix="poc-mlops-platform/memory",
         ),
     }
 
 defs = dg.Definitions(
-    assets=assets,
+    assets=list(translator.named_assets.values()),
     resources=resources,
-    jobs=jobs,
-    schedules=schedules,
-    loggers=loggers,
-    executor=dg.multiprocess_executor(max_concurrent=2),
+    jobs=list(translator.named_jobs.values()),
+    schedules=list(translator.named_schedules.values()),
+    sensors=list(translator.named_sensors.values()),
+    loggers=translator.named_loggers,
+    executor=dg.multiprocess_executor.configured(dict(max_concurrent=2)),
 )
