68dfa8d0bcfbba9f32df355035d89a3f1824015a
Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>
2025-03-05 17:29:53 +0100

    Minor change
    Signed-off-by: Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>

--- src/kedro_dagster/templates/definitions.py
+++ src/kedro_dagster/templates/definitions.py
@@ -3,7 +3,6 @@
 import os
 
 import dagster as dg
-from dagster_aws.s3 import S3PickleIOManager, S3Resource
 
 from kedro_dagster import KedroDagsterTranslator
 
@@ -14,18 +13,9 @@ translator.translate()
 
 resources = translator.named_resources
 # The "io_manager" key handles how Kedro MemoryDatasets are handled by Dagster
-if KEDRO_ENV == "base":
-    resources |= {
-        "io_manager": dg.fs_io_manager,
-    }
-elif KEDRO_ENV == "dev":
-    resources |= {
-        "io_manager": S3PickleIOManager(
-            s3_resource=S3Resource(),
-            s3_bucket="zaphiro-dagster-demo",
-            s3_prefix="poc-mlops-platform/memory",
-        ),
-    }
+resources |= {
+    "io_manager": dg.fs_io_manager,
+}
 
 defs = dg.Definitions(
     assets=list(translator.named_assets.values()),
